0d70de1d19434e426e0574b142f1fa43182a84bf
rigevit <velles@meta.ua>
2016-03-22 11:30:35 +0200

    compareTo

--- JavaRushHomeWork/src/com/javarush/test/level08/lesson11/home09/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/level08/lesson11/home09/Solution.java
@@ -3,8 +3,7 @@ package com.javarush.test.level08.lesson11.home09;
 import java.util.Date;
 
 /* Работа с датой
-1. Реализовать метод isDateOdd(String date) так, чтобы он возвращал true,
-если количество дней с начала года - нечетное число, иначе false
+1. Реализовать метод isDateOdd(String date) так, чтобы он возвращал true, если количество дней с начала года - нечетное число, иначе false
 2. String date передается в формате MAY 1 2013
 Не забудьте учесть первый день года.
 Пример:
@@ -16,10 +15,25 @@ public class Solution
 {
     public static void main(String[] args)
     {
+//        String d = "JANUARY 2 2022";
+//        System.out.println(d + " " + isDateOdd(d));
     }
 
     public static boolean isDateOdd(String date)
     {
-        return true;
+        Date date2 = new Date(date);
+        Date date1 = new Date();
+        date1.setSeconds(0);
+        date1.setMinutes(0);
+        date1.setHours(0);
+
+        date1.setDate(1);
+        date1.setMonth(0);
+        date1.setYear(date2.getYear());
+        long msDay = 24 * 60 * 60 * 1000;
+        long raznica = date2.getTime() + msDay - date1.getTime();
+        int daycount = (int)(raznica/msDay);
+
+        return ((daycount%2==0)?((true)):((false)));
     }
 }
